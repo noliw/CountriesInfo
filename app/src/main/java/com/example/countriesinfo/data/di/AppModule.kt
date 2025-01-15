db100aabc8050a207ea50ac9424d5fe4f12f3f58
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-01-15 02:27:33 -0800

    adding the app module

---
+++ app/src/main/java/com/example/countriesinfo/data/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.countriesinfo.data.di
+
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Singleton
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
